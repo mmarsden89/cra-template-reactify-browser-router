023bebceeff45ca979ffa558e7bc12007e7b237f
mmarsden89 <mattmarsdendev@gmail.com>
2021-12-21 18:31:26 -0500

    styling/refactor of hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,12 +5,14 @@ const Hero = () => {
   return (
     <div className="hero-container">
       <ImageCycle />
-      <h1>thanks for choosing Reacticious</h1>
-      <div className="subtext">
-        <p>
-          feel free to edit the code and publish however you please.
-          documentation can be found&nbsp;<a href="#">here</a>
-        </p>
+      <div className="text">
+        <h1>thanks for choosing Reacticious</h1>
+        <div className="subtext">
+          <p>feel free to edit the code and publish however you please</p>
+          <p>
+            documentation can be found&nbsp;<a href="#">here</a> :)
+          </p>
+        </div>
       </div>
     </div>
   );
